abbb0a2336f370f8397c25aede92b5d8c47d7765
Ashik Hebbar <akvhebbar@gmail.com>
2025-12-07 20:35:04 +0530

    Add files via upload

---
+++ Qrcode.js
@@ -0,0 +1,14 @@
+// QrCode.js
+import React from 'react';
+import QRCodeGenerator from '../components/QRCodeGenerator';
+
+const QrCode = () => {
+  return (
+    <div>
+      <h2>QR Code Generator</h2>
+      <QRCodeGenerator />
+    </div>
+  );
+};
+
+export default QrCode;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# HaQR
+this is a website where you can generate dynamic qrcode with live analytics and many more modificatoins on your qrcode
